14f886f7bdce8628de2b612929f4023d6752881d
buheracoupe <176396718+buheracoupe@users.noreply.github.com>
2026-09-16 08:35:26 +0200

    style: load Stage 2 refinements

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/index.css'
+import './styles/stage2-refinements.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
